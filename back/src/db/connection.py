3764754402b01054e1e701b9acc480112fb02aa0
gastón_Alonso() <tonga88@live.com.ar>
2025-09-14 20:12:15 -0300

    configuracion de archivo .env

--- back/src/db/connection.py
+++ back/src/db/connection.py
@@ -1,16 +1,17 @@
-from errno import errorcode
+from dotenv import load_dotenv
+import os
 import mysql.connector
 
-
+load_dotenv()
 
 def get_connection():
     try:
         return mysql.connector.connect(
-            user= 'your-username',
-            password='your-password',
-            host='your-host',
-            database='your-data-base-name',
-            port='your-port'
+            user= os.getenv('DB_USER'),
+            password = os.getenv('DB_PASSWORD'),
+            host = os.getenv('DB_HOST'),
+            database = os.getenv('DB_NAME'),
+            port = os.getenv('DB_PORT')
         )
     except mysql.connector.Error as err:
         print(f"Error al conectar: {err}")
