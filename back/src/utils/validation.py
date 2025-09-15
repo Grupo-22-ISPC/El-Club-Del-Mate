e945d562b0804ce7a51caf80cca628b77a110282
gastón_Alonso() <tonga88@live.com.ar>
2025-09-14 21:17:23 -0300

    validaciones nombre,email y contraseña

--- back/src/utils/validation.py
+++ back/src/utils/validation.py
@@ -1,15 +1,16 @@
-def validar_contrasena():
-    pass
+import re
 
+def validar_nombre(nombre: str) -> bool:
+    return len(nombre.strip()) >= 3
 
+def validar_email(email: str) -> bool:
+    return re.match(r"^[\w\.-]+@[\w\.-]+\.\w+$", email)
 
-
-def validar_email():
-    pass
-
-
-
-def validar_nombre():
-    pass
-
-
+def validar_contrasena(contrasena: str) -> bool:
+    return (
+        len(contrasena) >= 8 and
+        re.search(r"[A-Z]", contrasena) and
+        re.search(r"[a-z]", contrasena) and
+        re.search(r"[0-9]", contrasena) and
+        re.search(r"[!@#$%^&*(),.?\":{}|<>]", contrasena)
+    )
