6d6a51bf1642ae6b71b55154cb523130ddf21f8b
gastón_Alonso() <tonga88@live.com.ar>
2025-10-13 21:26:51 -0300

    actualiza llama SELECT a VIEW

--- back/src/db/pedido_dao.py
+++ back/src/db/pedido_dao.py
@@ -6,21 +6,8 @@ def obtener_pedidos_con_detalle(cliente):
     conn = None
     try:
         conn = get_connection()
-        cursor = conn.cursor(dictionary=True)
-        query = """
-            SELECT 
-                p.id_pedido,
-                p.fecha_pedido,
-                pr.nombre AS producto,
-                pp.cantidad,
-                pr.precio,
-                (pp.cantidad * pr.precio) AS total_por_producto
-            FROM pedido p
-            JOIN pedido_producto pp ON p.id_pedido = pp.id_pedido
-            JOIN producto pr ON pp.id_producto = pr.id_producto
-            WHERE p.id_usuario = %s
-            ORDER BY p.fecha_pedido DESC;
-        """
+        cursor = conn.cursor(dictionary=True)     
+        query = "SELECT * FROM vista_pedidos_detalles WHERE id_usuario = %s;"
         cursor.execute(query,(cliente.id,))
         return cursor.fetchall()
     except mysql.connector.Error as e:
@@ -31,13 +18,13 @@ def obtener_pedidos_con_detalle(cliente):
             conn.close()
 
 
-def crear_pedido(cliente, cantidad_total):
+def crear_pedido(cliente):
     conn = None
     try:
         conn = get_connection()
         cursor = conn.cursor()
-        query = "INSERT INTO pedido (fecha_pedido, cantidad, id_usuario) VALUES (CURRENT_DATE, %s, %s)"
-        cursor.execute(query, (cantidad_total, cliente.id))
+        query = "INSERT INTO pedido (fecha_pedido, id_usuario) VALUES (CURRENT_DATE, %s)"
+        cursor.execute(query, ( cliente.id,))
         conn.commit()
         return cursor.lastrowid  # devuelve id_pedido
     except mysql.connector.Error as e:
